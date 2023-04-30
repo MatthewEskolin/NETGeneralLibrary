2720a4f39cc299988e691210b9e959275f444818
Matthew Eskolin <matteskolin@gmail.com>
2023-04-30 09:30:27 -0500

    add first file

---
+++ NETGeneralLibrary/Utilities.cs
@@ -0,0 +1,15 @@
+﻿namespace NetGeneralLibrary.Utilities
+{
+    public static class CUtilities
+    {
+
+        public static bool IntToBool(int i)
+        {
+            if (i == 1) return true;
+            if (i == 0) return false;
+
+            //return default value;
+            return false;
+        }
+    }
+}
